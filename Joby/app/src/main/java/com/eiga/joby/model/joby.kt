668931a51069c5ba1cac8e4e2e311c8ac2e53f00
alvarisqi <101438298+alvarisqi@users.noreply.github.com>
2022-04-08 13:20:07 +0800

    Delete Joby directory

--- Joby/app/src/main/java/com/eiga/joby/model/joby.kt
+++
@@ -1,7 +0,0 @@
-package com.eiga.joby.model
-
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-
-data class joby(@StringRes val stringResourceId: Int, @DrawableRes val imageResourceId: Int) {
-}
